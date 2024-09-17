2431261dcc9558f84cba57f1a844b72e33019cda
nicojpalacio <nicojpalacio@gmail.com>
2024-09-17 01:10:33 -0300

    stated portfolio

---
+++ components/AboutMe.tsx
@@ -0,0 +1,96 @@
+'use client'
+import { TypeAnimation } from "react-type-animation";
+import React from 'react'
+import Image from "next/image";
+import { motion } from 'framer-motion';
+
+const AboutMe = () => {
+    return (
+        <section className='flex flex-col justify-start sm:flex-row items-center '>
+            {/* Imagen arriba del h1 en mobile y a la par en desktop */}
+            <motion.div 
+            initial={{opacity:0}}
+            animate={{
+                opacity:1,
+                transition:{delay:1, duration:0.4, ease:'easeIn'}
+            }}
+            className='relative p-5 sm:mb-0 sm:mr-8'>
+                <motion.div
+                    initial={{opacity:0}}
+                    animate={{
+                        opacity:1,
+                        transition:{delay:1.2, duration:0.4, ease:'easeInOut'}
+                    }}
+                >
+                    <Image
+                        src='/fotoperfilsinfondo.png'
+                        alt='image'
+                        width={200}
+                        height={200}
+                        className='rounded-3xl'
+                    />
+                </motion.div>
+
+                {/* SVG circle rotating around the image */}
+                <motion.svg
+                    className='absolute top-3 -left-14 w-[350px] h-[350px]'
+                    viewBox='0 0 500 500'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='transparent'
+                    /* animate={{ rotate: 360 }}
+                    transition={{
+                        duration: 20,
+                        ease: 'linear',
+                        repeat: Infinity,
+                    }} */
+                >
+                    <motion.circle
+                        cx='250'
+                        cy='250'
+                        r='200'
+                        stroke='rgb(236 72 153)'
+                        strokeWidth='4'
+                        strokeDasharray='round'
+                        strokeLinecap='round'
+                        initial={{strokeDasharray:'24 10 0 0'}}
+                        animate={{
+                            strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+                            rotate:[120,360]
+                        }}
+                        transition={{
+                            duration:5,
+                            repeat:Infinity,
+                            repeatType:'reverse'
+                        }}
+                        fill='transparent'
+                    />
+                </motion.svg>
+            </motion.div>
+
+            <div className='flex flex-col items-center text-center sm:text-left'>
+                <h1 className='text-white pt-6 mb-4 text-3xl sm:text-4xl lg:text-5xl lg:leading-normal font-extrabold z-10'>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500'>
+                        Hola, mi nombre es Nicolas Palacio{" "}
+                    </span>
+                    <br />
+                    <TypeAnimation
+                        sequence={[
+                            "Desarrollador Frontend",
+                            1000,
+                            "Desarrollador Backend",
+                            1000,
+                            "Diseñador UI/UX",
+                            1000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={3}
+                        style={{ display: 'inline-block' }}
+                    />
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default AboutMe;
---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import logofinca from '@/public/logofinca.jpg'
+import Image from 'next/image'
+
+const Hero = () => {
+    return (
+        <div className='pb-10 pt-20'>
+            <div>
+                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
+                <Spotlight className='hidden md:block top-10 left-full h-[80vh] w-[50vw]' fill='orange' />
+                <Spotlight className='hidden md:block top-28 left-80 h-[80vh] w-[50vw]' fill='red' />
+            </div>
+            <div className="h-[28rem] w-full dark:bg-black bg-purple-900 dark:bg-dot-white/[0.3] bg-dot-black/[0.3] flex items-center justify-center absolute top-0 left-0 z-0">
+                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-purple-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+            </div>
+
+            <div className='flex justify-center  my-20 z-10'>
+                <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                    
+                    
+                    <h2 className='tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                        Porfolio desarrollado con Next.js
+                    </h2>
+                    <TextGenerateEffect
+                        className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                        words='Full Stack Developer'
+                    />
+                   {/*  <div className='m-5 '>
+                        <Image
+                            src='/logofinca.jpg'
+                            alt='image'
+                            width={200}
+                            height={200}
+                            className='rounded-full'
+                        />
+                    </div> */}
+                    <button className='relative px-6 py-3 mt-5 w-full sm:w-fit rounded-full text-white cursor-pointer z-0 overflow-hidden group'>
+                        <span className='absolute inset-0 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 rounded-full group-hover:from-pink-500 group-hover:via-purple-500 group-hover:to-blue-500'></span>
+                        <span className='relative z-10'>Descarga mi CV</span>
+                    </button>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
